b70c0057223a7fc26d73e42762269bb2bc0c736a
andrew <burns.andrew@hotmail.co.uk>
2019-05-13 12:55:49 +0100

    initial seeding has started

--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -1,10 +1,20 @@
-// const {  } = require('../data');
+// const { } = require("../../utils")
+// this will be for when I use a reference object and a function to format the data
 
-exports.seed = (knex, Promise) => {
-  return knex.migrate
+const {
+  topicsData,
+  articlesData,
+  usersData,
+  commentsData
+} = require("../data");
+
+exports.seed = (connection, Promise) => {
+  return connection.migrate
     .rollback()
-    .then(() => knex.migrate.latest())
+    .then(() => connection.migrate.latest())
     .then(() => {
-      // insert data
+      return connection("topics")
+        .insert(topicsData)
+        .returning("*");
     });
 };
