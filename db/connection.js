885c09f4c8148ba050977db7fc3dd63dc5453cf8
andrew <burns.andrew@hotmail.co.uk>
2019-05-23 09:20:39 +0100

    heroku hosting set up completed

--- db/connection.js
+++ db/connection.js
@@ -1,6 +1,9 @@
+const ENV = process.env.NODE_ENV || "development";
 const knex = require("knex");
-const dbConfig = require("../knexfile");
 
-const connection = knex(dbConfig);
+const dbConfig =
+  ENV === "production"
+    ? { client: "pg", connection: process.env.DATABASE_URL }
+    : require("../knexfile");
 
-module.exports = connection;
+module.exports = knex(dbConfig);
--- db/data/index.js
+++ db/data/index.js
@@ -1,11 +1,12 @@
-const ENV = process.env.NODE_ENV || 'development';
+const ENV = process.env.NODE_ENV || "development";
 
-const development = require('./dev-data');
-const test = require('./test-data');
+const development = require("./dev-data");
+const test = require("./test-data");
 
 const data = {
   development,
   test,
+  production: development
 };
 
 module.exports = data[ENV];
