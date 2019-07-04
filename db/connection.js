c383ad2d7dd7b86382a8ab8dc0a64e45e12488c9
andrew <burns.andrew@hotmail.co.uk>
2019-07-04 13:45:40 +0100

    attempt to fix database error connection

--- db/connection.js
+++ db/connection.js
@@ -2,7 +2,7 @@ const ENV = process.env.NODE_ENV || "development";
 const knex = require("knex");
 const dbConfig =
   ENV === "production"
-    ? { client: "pg", connection: process.env.DB_URL }
+    ? { client: "pg", connection: process.env.DATABASE_URL }
     : require("../knexfile.js");
 
 module.exports = knex(dbConfig);
